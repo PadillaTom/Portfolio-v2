94dee251c992277705b1f1f3bc4a083f9fe51601
Tomas Padilla <padillatomasagustin@gmail.com>
2020-11-09 15:46:02 +0100

    Formato Tablet, Contact Section

--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -40,32 +40,34 @@ const AboutPage = () => {
             <h2>About me</h2>
           </div>
           <div className='about-info-container'>
-            <div className='about-info'>
-              <p>
-                With an artistic background, I have started my studies as a Web
-                Developer to find a real passion.
-              </p>
-              <br />
-              <span>Tools & Technologies:</span>
-              <br />
-              <p>
-                HTML, CSS, Javascript, Gsap, React, Gatsby, NPM, Git, Firebase,
-                Material UI, Jquery.
-              </p>
-              <br />
-              <span>How will I proceed?</span>
-              <br />
-              <p>PHP - mySQL - MongoDB</p>
-              <br />
-              <p>
-                I invite you to see my
-                <button className='proj-cta'>Projects.</button>
-              </p>
-            </div>
-            <div className='about-cta'>
-              <Link to='/contact'>
-                <p>Contact Me</p>
-              </Link>
+            <div className='about-info-center'>
+              <div className='about-info'>
+                <p>
+                  With an artistic background, I have started my studies as a
+                  Web Developer to find a real passion.
+                </p>
+                <br />
+                <span>Tools & Technologies:</span>
+                <br />
+                <p>
+                  HTML, CSS, Javascript, Gsap, React, Gatsby, NPM, Git,
+                  Firebase, Material UI, Jquery.
+                </p>
+                <br />
+                <span>How will I proceed?</span>
+                <br />
+                <p>PHP - mySQL - MongoDB</p>
+                <br />
+                <p>
+                  I invite you to see my
+                  <button className='proj-cta'>Projects.</button>
+                </p>
+              </div>
+              <div className='about-cta'>
+                <Link to='/contact'>
+                  <p>Contact Me</p>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
